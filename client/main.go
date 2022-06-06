2da82eb295326edcd38e625a1dbcf4a336f14226
Vikash Parashar <99866771+gowithvikash@users.noreply.github.com>
2022-06-06 11:40:10 +0530

    my second commit

--- client/main.go
+++ client/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	pb "github.com/gowithvikash/grpc_with_go/grpc_unary_api/proto"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -9,9 +13,17 @@ var (
 )
 
 func main() {
+	conn, err := grpc.Dial(address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	c := pb.NewGreetServiceClient(conn)
+	do_Simple_Greet(c)
 
 }
 
 func do_Simple_Greet(c pb.GreetServiceClient) {
+	fmt.Println("do_Simple_Greet Function Was Invoked At Client")
 
 }
--- server/main.go
+++ server/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
 	pb "github.com/gowithvikash/grpc_with_go/grpc_unary_api/proto"
+	"google.golang.org/grpc"
 )
 
 type Server struct {
@@ -14,5 +20,18 @@ var (
 )
 
 func main() {
+	lis, err := net.Listen(network, address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreetServiceServer(s, &Server{})
+	if err = s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 
+}
+func (s *Server) Simple_Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
+	fmt.Println("___ Simple_Greet Function Was Invoked At Client___")
+	return &pb.GreetResponse{Result: fmt.Sprintf("Hello And Welcome %s !\n", in.Name)}, nil
 }
